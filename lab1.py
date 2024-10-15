ae335887c9826aa33fb1908ff866162c982aa2d5
AnastasiaVitiv <vitiv.tuska@gmail.com>
2024-10-15 15:38:18 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,16 @@
+animal = "cat"
+print(animal)
+
+is_weekend = True
+is_workday = False
+print(is_workday)
+
+is_mammal = True
+has_wings = False
+print(is_mammal)
+
+import math
+x = 4.112
+y = 1.628
+result = (((3 * (x ** 2)) + 1)/(5 * y)) + ((2 * math.tan(x + 1))/(y - 2))
+print(result)
